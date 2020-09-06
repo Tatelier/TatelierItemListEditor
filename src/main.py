d81a8a0e6ee63d5721c8fcc979255462605ae024
Pansystar <wh.bt3.p@gmail.com>
2020-09-07 06:48:21 +0900

    初期コミット

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 import eel
+import glob
 
-scoreDir = ""
+scoreDir = "C:\\Users\\whbt3\\MyDrive\\MyDevelop\\C#\\Tatelier\\Tatelier\\bin\\Debug\\Resources\\Score"
+
+
+
+@eel.expose
+def python_function():
+    return glob.glob('{}/**/*.tja'.format(scoreDir), recursive=True)
+
 
 eel.init('web')
-eel.start('index.html', port=8008)
+eel.start('index.html', port=8018)
